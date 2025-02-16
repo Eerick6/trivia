f9696b9d016291b877cd2e6224a8a6b8b0991d8b
Eerick6 <erick199335@gmail.com>
2025-02-16 11:00:24 -0500

    triviav1

---
+++ config/db.php
@@ -0,0 +1,25 @@
+<?php
+class Database {
+    private $host = "localhost";  // Host de la base de datos
+    private $db_name = "trivia";  // Nombre de la base de datos
+    private $username = "root";  // Usuario de la base de datos
+    private $password = "";  // Contraseña del usuario de la base de datos
+    private $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            // Establecer la conexión utilizando PDO
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, 
+                                  $this->username, 
+                                  $this->password);
+            // Configurar el modo de errores de PDO
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            // Capturar el error y mostrarlo si la conexión falla
+            echo "Error al conectar a la base de datos: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
